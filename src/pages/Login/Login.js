c2a95ecf5bc2f813b49e00fb2bf01413cfd1d867
Larisoft01 <19.02028-7@maua.br>
2024-06-30 00:51:39 -0300

    front

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Input from "../../components/Input/input";
+import Button from "../../components/Button/button";
+import * as C from "./styles";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+const Signin = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if (!email | !senha) {
+      setError("Preencha todos os campos");
+      return;
+    }
+
+    const res = signin(email, senha);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    navigate("/UserProfile");
+  };
+
+  return (
+    <C.Container>
+      <C.Label>Entre na Sua Conta</C.Label>
+      <C.Content>
+        <Input
+          type="email"
+          placeholder="Digite seu E-mail"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua Senha"
+          value={senha}
+          onChange={(e) => [setSenha(e.target.value), setError("")]}
+        />
+        <C.labelError>{error}</C.labelError>
+        <Button Text="Entrar" onClick={handleLogin} />
+        <C.LabelSignup>
+          Não tem uma conta?
+          <C.Strong>
+            <Link to="/signup">&nbsp;<span>Registre-se</span></Link>
+          </C.Strong>
+        </C.LabelSignup>
+      </C.Content>
+    </C.Container>
+  );
+};
+
+export default Signin;
